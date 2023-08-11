074bc5d58c830c0d1e229fa100906d47bb3a50d4
Cai Zhi Bin <68769529+czhi-bin@users.noreply.github.com>
2023-08-11 23:56:36 +0800

    Set up Hertz server

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"github.com/czhi-bin/mini-tiktok-backend/biz/dal/db"
-
+	"context"
 	"fmt"
+	
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/czhi-bin/mini-tiktok-backend/biz/dal/db"
 )
 
 func main() {
 	db.Init()
 	fmt.Println("DB init success")
+
+	h := server.Default(server.WithHostPorts("localhost:8080"))
+	
+	h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+			ctx.JSON(consts.StatusOK, utils.H{"message": "pong"})
+	})
+
+	h.Spin()
 }
